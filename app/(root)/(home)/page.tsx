e98e2e1c5e5b4cb4461a2f63bc1155655d36a560
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-09-21 15:23:49 -0400

    removed console.log

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -19,8 +19,6 @@ const Page = async ({ searchParams }: Props) => {
 
   const resourcesPlaylist = await getResourcesPlaylist();
 
-  console.log(resourcesPlaylist);
-
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
